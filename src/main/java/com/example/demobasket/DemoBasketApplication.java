e8a9eeb9790e2ddcf26a4bc8127a763cd2ffafe5
RomanPustovalov <120693916+RomanPustovalov@users.noreply.github.com>
2023-05-05 07:23:27 +0300

    initial

---
+++ src/main/java/com/example/demobasket/DemoBasketApplication.java
@@ -0,0 +1,13 @@
+package com.example.demobasket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoBasketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoBasketApplication.class, args);
+	}
+
+}
